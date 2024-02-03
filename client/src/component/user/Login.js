91cfb364369fa70bcecaf5345df69c5daf81816b
Allen Cortuna <allen@Allens-MacBook-Air.local>
2024-02-03 12:51:05 +0800

    log layout changes

--- client/src/component/user/Login.js
+++ client/src/component/user/Login.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userStore } from "../../zustand/user";
-import { BiUserCircle } from "react-icons/bi";
-import { BsShieldLock } from "react-icons/bs";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -21,26 +19,37 @@ const Login = () => {
   };
 
   const input =
-    "border-2 focus:border-cyan-800 border-zinc-300 text-zinc-500 font-[500] text-lg p-3  rounded-md w-full bg-none my-4 focus:outline-none focus:border-cyan-700";
+    "border-2 focus:border-cyan-800 border-zinc-300 text-zinc-500 font-[500] text-sm p-3 px-2  rounded-md w-full bg-none my-4 focus:outline-none focus:border-cyan-700";
   return (
-    <span className="w-screen h-screen flex">
-      <form className="m-auto grid bg-white shadow-lg m-auto min-w-[23rem] max-w-[26rem] h-auto border-2 border-zinc-300 rounded-2xl">
-        <h4 className="font-semibold text-white text-3xl text-center mb-5 bg-zinc-700 p-5 rounded-tr-xl rounded-tl-xl ">
-          Login
-        </h4>
-        <span className="m-auto grid rounded-xl p-5">
-          <p className="text-lg text-zinc-600 mt-5 font-[500] ">
-            <BiUserCircle className="text-xl ml-2 mr-1 inline lato" /> Username
-          </p>
+    <span className="w-screen h-screen flex flex-col">
+      {/* Introduction */}
+      <span className="mx-auto mt-20 mb-14 flex flex-col gap-3 ">
+
+        <span className="flex mx-auto gap-1">
+          <h1 className="text-3xl font-bold text-blue-500 text-center shadow-md p-2 rounded-md border">
+            FILE
+          </h1>
+          <h1 className="text-3xl font-bold text-zinc-700 p-2 text-center">
+            Track
+          </h1>
+        </span>
+
+        <p className="text-zinc-500 text-sm">
+          Document Information and Status Web application{" "}
+        </p>
+      </span>
+
+      {/* login form */}
+      <form className="mx-auto grid bg-white shadow-lg min-w-[23rem] max-w-[26rem] h-auto border rounded-2xl">
+        <span className="m-auto grid rounded-xl p-5 mt-8">
+          <p className="text-sm text-zinc-600 font-[500] ">Enter Username</p>
           <input
             type="text"
             name="username"
             onChange={handleChange}
             className={input}
           />
-          <p className="text-lg text-zinc-600 mt-5 font-[500]">
-            <BsShieldLock className="text-xl ml-2 mr-1 inline lato" /> Password
-          </p>
+          <p className="text-sm text-zinc-600  font-[500]">Enter Password</p>
           <input
             type="password"
             name="password"
@@ -49,7 +58,7 @@ const Login = () => {
           />
 
           <h4
-            className="underline text-sm my-4 ml-4 text-zinc-500"
+            className="underline text-xs my-4 text-zinc-500"
             onClick={() => navigate("/reset")}
           >
             Forgot Password?
@@ -58,12 +67,12 @@ const Login = () => {
           <button
             type="submit"
             disabled={loading}
-            className={`p-4 px-8 font-[500] text-md border-2 rounded-lg bg-zinc-700 text-white w-full mt-8 m-auto  ${
+            className={`p-4 font-[500] text-sm border-2 rounded-full bg-zinc-700 text-white w-full mt-8 m-auto  ${
               loading && "animate-pulse"
             }`}
             onClick={handleSubmit}
           >
-            {loading ? "Processing" : "Login"}
+            {loading ? "Processing" : "Submit"}
           </button>
         </span>
       </form>
